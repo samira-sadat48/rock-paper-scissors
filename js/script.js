f394b39a9d9c30bd1f31862b98ca0044835d69c8
samira-sadat48 <samira.sadat48@gmail.com>
2021-08-06 09:52:13 -0600

    Added a function for the computer's play - not random yet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+//Computer Play
+function computerPlay()
+{
+    let computerSelection = 0; //random number 0-2
 
+    switch(computerSelection)
+    {
+        case 0 :
+            return "rock";
+            break;
+        case 1 :
+            return "paper";
+            break;
+        case 2 :
+            return "scissors";
+            break;
+        default:
+            return "Not a valid selection";//throw an error?
+    }
+}
 
-console.log("This is the Rock Paper Scissors Game!");
+console.log(computerPlay());
